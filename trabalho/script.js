e2b63619eed082b392ecffb247c38bbfd462d6e1
Junior-Leonel <157860296+Junior-Leonel@users.noreply.github.com>
2025-05-06 11:49:03 -0300

    Delete trabalho directory

--- trabalho/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mensagemBtn").addEventListener("click", function() {
-    const mensagem = document.getElementById("mensagem");
-    mensagem.textContent = "Nunca desista dos seus sonhos! ";
-      });
