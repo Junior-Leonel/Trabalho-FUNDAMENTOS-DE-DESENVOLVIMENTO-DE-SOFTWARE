305740348380e3afacf3a0fbaaaab2504a7bfc06
Junior-Leonel <157860296+Junior-Leonel@users.noreply.github.com>
2025-05-06 11:49:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("mensagemBtn").addEventListener("click", function() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Nunca desista dos seus sonhos! ";
+      });
